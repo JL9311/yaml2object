1a57a81ae8e56eb93136bd1b683d55e26628e785
耿军琳 <1373557856@qq.com>
2024-01-19 15:39:17 +0800

    注释

--- config_reader.py
+++ config_reader.py
@@ -1,9 +1,15 @@
 import yaml
 import json
-import configparser
 
 
 def field(name, required=False):
+    """
+    字段装饰器，用于标记字段的名称和是否必须
+    :param name: 在配置文件中的名称，如果是嵌套的，用.分割
+    :param required: 是否必须
+    :return: 装饰器
+    """
+
     def decorator(func):
         func.name = name
         func.required = required
@@ -13,6 +19,12 @@ def field(name, required=False):
 
 
 def yaml_to_object(class_name, yaml_config_str):
+    """
+    将yaml配置转换为对象
+    :param class_name: 类
+    :param yaml_config_str: yaml配置
+    :return: 对象
+    """
     config_yaml = yaml.safe_load(yaml_config_str)
     config_obj = class_name()
 
@@ -44,6 +56,12 @@ def yaml_to_object(class_name, yaml_config_str):
 
 
 def json_to_object(class_name, json_str):
+    """
+    将json配置转换为对象
+    :param class_name: 类
+    :param json_str: json配置
+    :return: 对象
+    """
     json_dict = json.loads(json_str)
     json_obj = class_name()
 
@@ -63,7 +81,6 @@ def json_to_object(class_name, json_str):
         # 读取 json，name是json的key，按.分割，required是是否必须
         # 读取json的值，赋值给application_config
         # 如果required是True，但是json中没有这个key，抛出异常
-        # 如果required是False，但是json中没有这个key，使用default的值
         name_list = name.split('.')
         # 是用递归的方式，一层一层的读取json
         value = read_json(name_list, json_dict)
@@ -75,4 +92,3 @@ def json_to_object(class_name, json_str):
             json_obj.__setattr__(item, None)
 
     return json_obj
-
